5a21fcfcd07df244b17b55db6bb799b9b4c63e9c
Ysmael Marinho <ysmaeljunior@gmail.com>
2023-03-14 17:25:38 -0300

    04 - cenario mockado da api de CEP utilizadno mockito

---
+++ src/test/java/br/com/desafiosebrae/bdd/RunCucumberTests.java
@@ -0,0 +1,12 @@
+package br.com.desafiosebrae.bdd;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty" }, features = "src/test/resources")
+public class RunCucumberTests {
+    
+}
