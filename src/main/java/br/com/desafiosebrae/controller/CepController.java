cef2e45a982802e5614f447f1b732443d7a1efb1
Ysmael Marinho <ysmaeljunior@gmail.com>
2023-03-14 17:36:07 -0300

    05 - criacao do dockerfile e ajuste no endereco do endpoint cep

--- src/main/java/br/com/desafiosebrae/controller/CepController.java
+++ src/main/java/br/com/desafiosebrae/controller/CepController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.desafiosebrae.dto.CepDTO;
 import br.com.desafiosebrae.service.CepService;
 
 @RestController
+@RequestMapping("/cep")
 public class CepController {
     
     @Autowired
