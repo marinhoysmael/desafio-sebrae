e4a6119a99dba323f7b717faedc4ddbdab511ad5
Ysmael Marinho <ysmaeljunior@gmail.com>
2023-03-14 14:38:28 -0300

    Commit inicial - estrutura do projeto

---
+++ src/main/java/br/com/desafiosebrae/DesafioSebraeApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiosebrae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSebraeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSebraeApplication.class, args);
+	}
+
+}
